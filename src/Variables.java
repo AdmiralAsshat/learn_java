fca280ba65ff7eebd3f5fe4d343ebdd877e3df15
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-09-06 15:34:53 -0400

    Lecture 15: Declaring Variables

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables
+{
+	public status void main(String[] args)
+	{
+		int number;
+		double rate;
+		String firstName;
+		char letter;
+		boolean flag;
+
+		int salary, hoursWorked;
+		String middleName, lastName, initals;
+	}
+}
