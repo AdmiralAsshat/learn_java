85275d9470115411d6b64093dedcdbb2a0cbf5a2
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-09-06 15:42:14 -0400

    Lecture 16: Assigning Data to Variables

--- src/Variables.java
+++ src/Variables.java
@@ -2,13 +2,18 @@ public class Variables
 {
 	public status void main(String[] args)
 	{
-		int number;
-		double rate;
+		int number = 100;
+		double rate = 2.34;
 		String firstName;
 		char letter;
 		boolean flag;
 
 		int salary, hoursWorked;
 		String middleName, lastName, initals;
+
+		firstName = "Jane"; // assign value
+		number = 25 * 5;
+		letter = 'a';
+		flag = true;
 	}
 }
