94aba1d73991255aef6cf090d6bdf90ce40f27cb
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-09-07 23:17:12 -0400

    Lecture 28: Mixed Math Types

---
+++ src/MixedMath.java
@@ -0,0 +1,14 @@
+public class MixedMath
+{
+	public static void main(String[] args)
+	{
+		double dnum;
+		int num = 2;
+		dnum = num;
+		System.out.println(dnum);
+
+		char c = 'z';
+		num = c;
+		System.out.println(num);
+	}
+}
