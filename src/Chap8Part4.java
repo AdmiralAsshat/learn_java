cfbecbeafe6e16d104dcf54728a699f1bc0e18a5
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-09-11 22:11:41 -0400

    Lecture 38: Nested If-Else Statements

---
+++ src/Chap8Part4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Chap8Part4
+{
+	public static void main(String[] args)
+	{
+		int grade;
+		String letterGrade;
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a grade: ");
+		grade = input.nextInt();
+
+		if (grade >= 90)
+		{
+			if (grade >= 95)
+			{
+				letterGrade = "A+";
+				System.out.println("Your grade is: " + letterGrade);
+			}
+			if (grade >= 93)
+			{
+				letterGrade = "A";
+				System.out.println("Your grade is: " + letterGrade);
+			}
+			else
+			{	
+				letterGrade = "A-";
+				System.out.println("Your grade is: " + letterGrade);
+			}
+		}
+		else
+			System.out.println("Sorry, you didn't make an A.");
+	}
+}
