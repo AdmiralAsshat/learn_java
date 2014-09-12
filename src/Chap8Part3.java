258dd7176c54d6db43d943098a00dba97ecf6772
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-09-11 22:01:09 -0400

    Lecture 37: If-Else Statement

---
+++ src/Chap8Part3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Chap8Part3
+{
+	public static void main(String[] args)
+	{
+		int hoursWorked;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter hours worked: ");
+		hoursWorked = input.nextInt();
+		double rate = 25.00;
+		double grossPay;
+
+		if (hoursWorked > 40)
+		{
+			grossPay = (40 * rate) + ((hoursWorked - 40) * (rate * 1.5));
+		} else {
+			grossPay = hoursWorked * rate;
+		}
+
+		System.out.println("The gross pay is " + grossPay);
+	}
+}
