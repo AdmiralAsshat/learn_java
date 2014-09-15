a1a07ade412e4f074aaf104e75ee4a39ffdd3572
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-09-14 22:12:23 -0400

    Lecture 44: Count-Controlled Loops

---
+++ src/Chap9Part2.java
@@ -0,0 +1,18 @@
+public class Chap9Part2
+{
+	public static void main(String[] args)
+	{
+		double balance = 5000;
+		double rate = 1.02;
+		int year = 1;
+
+		while (year <= 10)
+		{
+			balance *= rate;
+			System.out.println("After year " + year + ", the balance is " + balance);
+			++year;
+		}
+
+		System.out.println("After 10 years the balance is " + balance + ".");
+	}
+}
