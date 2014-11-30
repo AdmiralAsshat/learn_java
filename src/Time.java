4ffa3a493d556357b4580efa29ecff34e0ea797d
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-11-30 13:04:42 -0500

    Lecture 72: More on constructors

--- src/Time.java
+++ src/Time.java
@@ -8,4 +8,27 @@ public class Time
 		minutes = 9;
 		seconds = 0;	
 	}
+
+	Time(int h, int m, int s)
+	{
+		if (h < 0 || h > 23)
+			hours = 0;
+		else
+			hours = h;
+		if (m < 0 || m > 60)
+			minutes = 0;
+		else
+			minutes = m;
+		if (s < 0 || s > 60)
+			seconds = 0;
+		else
+			seconds = s;
+	}
+
+	Time(int h, int m)
+	{
+		hours = h;
+		minutes = m;
+		seconds = 0;
+	}
 }
