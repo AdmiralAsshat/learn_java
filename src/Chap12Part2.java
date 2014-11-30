8275dae8257ff0d856c1b729d699350f2b7610db
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-11-30 13:17:56 -0500

    Lecture 73: Displaying Class Data

---
+++ src/Chap12Part2.java
@@ -0,0 +1,8 @@
+public class Chap12Part2
+{
+	public static void main(String[] args)
+	{
+		Time theTime = new Time(1,2,0);
+		System.out.println(theTime.display());
+	}
+}
--- src/Time.java
+++ src/Time.java
@@ -31,4 +31,30 @@ public class Time
 		minutes = m;
 		seconds = 0;
 	}
+
+	public String toString()
+	{
+		return "Hours: " + hours + "\n" +
+				"Minutes " + minutes + "\n" + 
+				"Seconds " + seconds;
+	}
+
+	public String display()
+	{
+		String hrs, mins, secs;
+		if (hours < 10)
+			hrs = "0" + hours;
+		else
+			hrs = hours + "";
+		if (minutes < 10)
+			mins = "0" + minutes;
+		else
+			mins = minutes + "";
+		if (seconds < 10)
+			secs = "0" + seconds;
+		else
+			secs = seconds + "";
+
+		return hrs + ":" + mins + ":" + secs;
+	}
 }
