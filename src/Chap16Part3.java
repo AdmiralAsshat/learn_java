4c4d0495648eb5f4869418cbdbbe7239e39c44ac
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-12-04 23:16:05 -0500

    Lecture 100: Do-While Loop pt.1

---
+++ src/Chap16Part3.java
@@ -0,0 +1,12 @@
+public class Chap16Part3
+{
+	public static void main(String[] args)
+	{
+		int number = 1;
+		do
+		{
+			System.out.println(number);
+			++number;
+		} while (number <= 10);
+	}
+}
